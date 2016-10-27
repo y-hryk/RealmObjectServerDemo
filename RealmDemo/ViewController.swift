4eed8830b2a84823e0d6364c25f4279c34a975f7
y-hryk <dev.hy630823@gmail.com>
2016-10-27 16:29:29 +0900

    Initial commit

---
+++ RealmDemo/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  RealmDemo
+//
+//  Created by yamaguchi on 2016/10/27.
+//  Copyright © 2016年 h.yamaguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.white
+    }
+
+}
+
